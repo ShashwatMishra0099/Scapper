0b70d9023681c687d3fb896e6835ca26312d6e3d
ShashwatMishra0099 <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-04-05 08:33:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,16 +11,12 @@ chat_id = '7176712779'
 
 async def scrape_group(group_name):
     client = TelegramClient('anon', api_id, api_hash)
-
     await client.start()
-
     group_entity = await client.get_entity(group_name)
-
     participants = await client(GetParticipantsRequest(
         group=group_entity,
         filter=ChannelParticipantsSearch('')
     ))
-
     users = []
     for participant in participants.users:
         user = {
@@ -28,9 +24,7 @@ async def scrape_group(group_name):
             'username': participant.username
         }
         users.append(user)
-
     await client.disconnect()
-    
     return users
 
 def create_pdf(users, file_name):
@@ -46,20 +40,13 @@ async def send_pdf_via_bot(file_name):
         await client.send_file(chat_id, file_name)
 
 async def main():
-    async with TelegramClient('anon', api_id, api_hash) as client:
-        @client.on(events.NewMessage(pattern='/start', chats=[chat_id]))
-        async def start_handler(event):
-            # Start the bot functionality when /start command is received
-            group_name = input("Enter the name of the public Telegram group: ")
-            users = await scrape_group(group_name)
-            file_name = f"{group_name}_scraped_users.pdf"
-            create_pdf(users, file_name)
-            await send_pdf_via_bot(file_name)
-            os.remove(file_name)
-            print("Scraped user data has been saved as a PDF and sent via Telegram bot.")
-
-        # Run the client event loop
-        await client.run_until_disconnected()
+    group_name = os.environ.get("TELEGRAM_GROUP_NAME")
+    users = await scrape_group(group_name)
+    file_name = f"{group_name}_scraped_users.pdf"
+    create_pdf(users, file_name)
+    await send_pdf_via_bot(file_name)
+    os.remove(file_name)
+    print("Scraped user data has been saved as a PDF and sent via Telegram bot.")
 
 if __name__ == '__main__':
     import asyncio
